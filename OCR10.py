1db3f1ab23c217461267da7c9ed1f5976131429b
Vinny Carter <cartervinny05@gmail.com>
2020-12-13 00:07:16 -0500

    added Preview Window

--- OCR10.py
+++ OCR10.py
@@ -1,9 +1,15 @@
 from PyQt5 import QtCore as qtc, QtGui as qtg, QtWidgets as qtw
-import sys, os
+import sys, os, shutil
+import pdfplumber
+from pdf2image import convert_from_path
+import cv2
+import pytesseract
+import numpy as np
 # import img2pdf
 # from PIL import Image, ImageQt
 
 from textbox import Ui_MainWindowOCR
+from preview import Ui_Preview
 # from welcome3 import Ui_DialogWelcome
 
 
@@ -14,19 +20,220 @@ class MainWindow(qtw.QMainWindow, Ui_MainWindowOCR):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         #variables
-        self.filename = ""
-        self.openFile()
+        self.PDF_file = ""
+        self.numPages = 0
+        self.curPage = 1
+        self.tempPath = "\\OCR10temp"
+        self.save_file = ""
+        self.saved = True
+
         self.setupUi(self)
+        self.openFile()
+        
+        
 
         #connect to slots
-        self.actionQuit.triggered.connect (self.close)
+        self.actionQuit.triggered.connect (self.exit)
+        self.actionOCR_Page.triggered.connect (self.OCR_Page_selected)
+        self.textEdit_Main.textChanged.connect (self.textChanged)
+        # self.checkBox_Gray.stateChanged.connect (self.checkbox_clicked)
+        
 
+        self.move(100,100)
         self.show()
 
-    def openFile (self):
-        options = qtw.QFileDialog.Options()
-        self.filename, _ = qtw.QFileDialog.getOpenFileName(self,"File to OCR","", "PDF files (*.pdf);; All Files (*)", options=options)
+       
 
+    def openFile (self):
+        #choose PDF file to open
+        options = qtw.QFileDialog.Options()
+        filename = ""
+        filename, _ = qtw.QFileDialog.getOpenFileName(self,"File to OCR","", "PDF files (*.pdf);; All Files (*)", options=options)
+        if not filename:
+            return
+        self.PDF_file = filename
+        self.changePDF(self.PDF_file)
+
+    def changePDF (self, pdf_file):
+        #change new PDF
+        pdf = pdfplumber.open(pdf_file)
+        pages = pdf.pages
+        if len(pages) == 0:
+            return
+        self.numPages = len(pages)
+        pages = convert_from_path(pdf_file, 350)
+        if not os.path.isdir(self.tempPath):
+            os.mkdir(self.tempPath)
+        i = 1
+        for page in pages:
+            image_name = os.path.join(self.tempPath, "Page_" + str(i) + ".jpg")
+            page.save(image_name, "JPEG")
+            i = i+1
+        self.curPage = 1
+        self.showPage (self.curPage)
+###################################################
+####################################################
+###################################################
+    def showPage (self, pageNo):
+        #show pageNo in left pane(label_Image)
+        image_name = os.path.join(self.tempPath, "Page_" + str(pageNo) + ".jpg")
+        if not os.path.exists (image_name):
+            image_name = ":/newPrefix/mfmc logo 2015.jpg"
+        img = cv2.imread(image_name)
+        # height, width, channel = img.shape
+        # bytesPerLine = 3 * width
+        # if self.checkBox_Gray.isChecked() == True:
+        #     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        #     adaptive_threshold = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 85, 11)
+        #     img = adaptive_threshold
+        #     height, width = img.shape
+        #     bytesPerLine += 1
+
+        
+        
+        # qImg = qtg.QImage(img.data, width, height, bytesPerLine, qtg.QImage.Format_RGB888)
+        # print ("gray1")
+        # self.label_Image.setPixmap(qtg.QPixmap(qImg))
+        # print ("gray2")
+        
+        self.label_Image.setPixmap(qtg.QPixmap(image_name))
+
+    def tesseractPage (self, pageNo):
+        self.widget = Preview (self.tempPath, pageNo, Filt1_state = self.checkBox_Filt1.isChecked(), Filt2_state = self.checkBox_Filt2.isChecked())
+        self.widget.show()
+        # image_name = os.path.join(self.tempPath, "Page_" + str(pageNo) + ".jpg")
+        # if not os.path.exists (image_name):
+        #     return
+        # img = cv2.imread(image_name)
+        # kernel = np.ones((1,1), np.uint8)
+        # if self.checkBox_Filt2.isChecked() == True:
+        #     img = cv2.dilate(img, kernel, iterations=1)
+        #     img = cv2.erode(img, kernel, iterations=1)
+        #     img = cv2.GaussianBlur(img, (5,5), 0)
+        #     img = cv2.medianBlur(img,5)
+
+
+        # if self.checkBox_Filt1.isChecked() == True:
+        #     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        #     adaptive_threshold = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 85, 11)
+        #     img = adaptive_threshold
+        # text = str(pytesseract.image_to_string(img, config='--psm 6'))
+        # self.textEdit_Main.setText(text)
+        # self.textEdit_Main.insertPlainText(text)
+
+    def saveAs (self):
+        options = qtw.QFileDialog.Options()
+        filename = ""
+        filename, _ = qtw.QFileDialog.getSaveFileName(self,"Save to file", self.save_file, "text files (*.txt);; All Files (*)", options=options)
+        if filename:
+            self.save_file = filename
+            self.save()
+
+    def save (self):
+        if self.save_file == "":
+            self.saveAs()
+        else:
+            with open(self.save_file, "w", encoding='utf-8') as f2:
+                f2.write(self.textEdit_Main.toPlainText())
+                self.saved = True
+
+    def exit(self):
+        # shutil.rmtree("\OCR10temp", ignore_errors=True)
+        if self.saved == False:
+            self.save()
+        self.close()
+
+
+#####################
+# SLOTS             #
+#####################
+
+    def wheelEvent(self,event):
+        #change page shown when while scrolled
+        self.y = 0
+        delta = event.angleDelta().y()
+        self.y += (delta and delta // abs(delta))
+        if (self.curPage<self.numPages) and (self.y < 0):
+            self.curPage += 1 #negative y increase page no
+            self.showPage(self.curPage)
+        if (self.curPage>1) and (self.y>0):
+            self.curPage -= 1
+            self.showPage(self.curPage)
+ 
+    def OCR_Page_selected(self):
+        self.tesseractPage(self.curPage)
+
+    def keyPressEvent(self, e):
+        if e.key() == qtc.Qt.Key_Escape:
+            self.exit()
+
+    def closeEvent(self,e):
+        self.exit()
+
+    def textChanged(self):
+        self.saved = False
+
+
+##########################
+# PREVIEW WIDGET         #
+##########################
+
+
+class Preview(qtw.QWidget, Ui_Preview):
+    # Signals
+
+    def __init__(self, tempPath, pageNo=1, Filt1_state = False, Filt2_state = False, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        #variables
+        self.tempPath = tempPath
+        self.pageNo = pageNo
+        self.setupUi(self)
+        self.checkBox_Filt1.setCheckState(Filt1_state)
+        self.checkBox_Filt2.setCheckState(Filt2_state)
+
+        self.OCR()
+        
+
+        #connect to slots
+        self.checkBox_Filt1.stateChanged.connect(self.OCR)
+        self.checkBox_Filt1.stateChanged.connect(self.OCR)
+       
+
+
+    def OCR (self):
+        image_name = os.path.join(self.tempPath, "Page_" + str(self.pageNo) + ".jpg")
+        if not os.path.exists (image_name):
+            return
+        img = cv2.imread(image_name)
+        kernel = np.ones((1,1), np.uint8)
+        if self.checkBox_Filt2.isChecked() == True:
+            img = cv2.dilate(img, kernel, iterations=1)
+            img = cv2.erode(img, kernel, iterations=1)
+            img = cv2.GaussianBlur(img, (5,5), 0)
+            img = cv2.medianBlur(img,5)
+
+
+        if self.checkBox_Filt1.isChecked() == True:
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            adaptive_threshold = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 85, 11)
+            img = adaptive_threshold
+        text = str(pytesseract.image_to_string(img, config='--psm 6'))
+        self.textEdit_Main.setText(text)
+        if self.checkBox_Filt1.isChecked() == True:
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            adaptive_threshold = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 85, 11)
+            img = adaptive_threshold
+        text = str(pytesseract.image_to_string(img, config='--psm 6'))
+        print("here")
+        self.textEdit_Main.setText(text)
+        print("there")
+
+
+
+
+#####################
+# MAIN              #
+#####################
 if __name__ == '__main__':
     app = qtw.QApplication(sys.argv)
     window = MainWindow()
--- PDFPlumber.py
+++ PDFPlumber.py
@@ -3,80 +3,82 @@ import sys, os
 import pdfplumber
 from pdf2image import convert_from_path
 
-# pdf_file = r"zivkovic, stana.pdf"
-# pdf = pdfplumber.open(pdf_file)
+pdf_file = r"zivkovic, stana.pdf"
+pdf = pdfplumber.open(pdf_file)
 
-# pages = pdf.pages
-# # print(pages)
-# page = pdf.pages[0]
-# text = page.extract_text()
-# if len(str(text))<50:
-#     pages = convert_from_path(pdf_file, 350)
+pages = pdf.pages
+print(len(pages))
+# print(pages)
+page = pdf.pages[0]
+text = page.extract_text()
+if len(str(text))<50:
+    pages = convert_from_path(pdf_file, 350)
 
-#     i = 1
-#     for enum, page in pages:
-#         image_name = "Page_" + str(i) + ".jpg"  
-#         page.save(image_name, "JPEG")
-#         i = i+1    
+    i = 1
+    for page in pages:
+        # image_name = "Page_" + str(i) + ".jpg"  
+        # page.save(image_name, "JPEG")
+        print (i, page)
+        i = i+1    
 
-import cv2
-from PIL import Image
-from matplotlib import pyplot as plt
+# import cv2
+# from PIL import Image
+# from matplotlib import pyplot as plt
 
-def mark_region(image_path):
+# def mark_region(image_path):
     
-    im = cv2.imread(image_path)
+#     im = cv2.imread(image_path)
 
-    gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(gray, (9,9), 0)
-    thresh = cv2.adaptiveThreshold(blur,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV,11,30)
+#     gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+#     blur = cv2.GaussianBlur(gray, (9,9), 0)
+#     thresh = cv2.adaptiveThreshold(blur,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV,11,30)
 
-    # Dilate to combine adjacent text contours
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (9,9))
-    dilate = cv2.dilate(thresh, kernel, iterations=4)
+#     # Dilate to combine adjacent text contours
+#     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (9,9))
+#     dilate = cv2.dilate(thresh, kernel, iterations=4)
 
-    # Find contours, highlight text areas, and extract ROIs
-    cnts = cv2.findContours(dilate, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+#     # Find contours, highlight text areas, and extract ROIs
+#     cnts = cv2.findContours(dilate, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+#     cnts = cnts[0] if len(cnts) == 2 else cnts[1]
 
-    line_items_coordinates = []
-    for c in cnts:
-        area = cv2.contourArea(c)
-        x,y,w,h = cv2.boundingRect(c)
+#     line_items_coordinates = []
+#     for c in cnts:
+#         area = cv2.contourArea(c)
+#         x,y,w,h = cv2.boundingRect(c)
 
-        if y >= 600 and x <= 1000:
-            if area > 10000:
-                image = cv2.rectangle(im, (x,y), (2200, y+h), color=(255,0,255), thickness=3)
-                line_items_coordinates.append([(x,y), (2200, y+h)])
+#         if y >= 600 and x <= 1000:
+#             if area > 10000:
+#                 image = cv2.rectangle(im, (x,y), (2200, y+h), color=(255,0,255), thickness=3)
+#                 line_items_coordinates.append([(x,y), (2200, y+h)])
 
-        if y >= 2400 and x<= 2000:
-            image = cv2.rectangle(im, (x,y), (2200, y+h), color=(255,0,255), thickness=3)
-            line_items_coordinates.append([(x,y), (2200, y+h)])
+#         if y >= 2400 and x<= 2000:
+#             image = cv2.rectangle(im, (x,y), (2200, y+h), color=(255,0,255), thickness=3)
+#             line_items_coordinates.append([(x,y), (2200, y+h)])
 
 
-    return image, line_items_coordinates
+#     return image, line_items_coordinates
 
-import pytesseract
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+# import pytesseract
+# pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
-# load the original image
-image, line_items_coordinates = mark_region('Page_1.jpg')
-image = cv2.imread('Page_1.jpg')
+# # load the original image
+# image, line_items_coordinates = mark_region('Page_1.jpg')
+# image = cv2.imread('Page_1.jpg')
 
-# get co-ordinates to crop the image
-for j in range(len(line_items_coordinates)):
-    c = line_items_coordinates[j]
+# # get co-ordinates to crop the image
+# for j in range(len(line_items_coordinates)):
+#     c = line_items_coordinates[j]
 
-    # cropping image img = image[y0:y1, x0:x1]
-    img = image[c[0][1]:c[1][1], c[0][0]:c[1][0]]    
+#     # cropping image img = image[y0:y1, x0:x1]
+#     img = image[c[0][1]:c[1][1], c[0][0]:c[1][0]]    
 
-    plt.figure(figsize=(10,10))
-    plt.imshow(img)
-    plt.show()
+#     plt.figure(figsize=(10,10))
+#     plt.imshow(img)
+#     plt.show()
 
-    # convert the image to black and white for better OCR
-    ret,thresh1 = cv2.threshold(img,120,255,cv2.THRESH_BINARY)
+#     # convert the image to black and white for better OCR
+#     ret,thresh1 = cv2.threshold(img,120,255,cv2.THRESH_BINARY)
 
-    # pytesseract image to string to get results
-    text = str(pytesseract.image_to_string(thresh1, config='--psm 6'))
-    print(text)
+#     # pytesseract image to string to get results
+#     text = str(pytesseract.image_to_string(thresh1, config='--psm 6'))
+#     print(text)
