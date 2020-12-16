7b68807f440c7a478014108cf85cc41504fecbea
VinnyCarter05 <vinnycarter05@gmail.com>
2020-12-16 08:47:35 -0500

    added directory.py

--- directory.py
+++ directory.py
@@ -1,12 +1,22 @@
 import os
 import datetime
 
-path = "faxes"
-PDFlist = []
-for file in os.listdir(path):
-    if file.lower().endswith(".pdf"):
-        PDFlist.append([file, os.path.getmtime(os.path.join(path,file))])
-    # if file.endswith(".txt"):
-    #     print(os.path.join("/mydir", file))
-for file, date in PDFlist:
-    print (file,datetime.datetime.fromtimestamp(date))
+
+
+def listdir_with_ext (path="", ext = ".*"):
+    PDFlist = []
+    for file in os.listdir(path):
+        if file.lower().endswith(ext):
+            PDFlist.append([file, os.path.getmtime(os.path.join(path,file))])
+    return sorted_list_by_col (PDFlist, 0)
+
+def sorted_list_by_col (lst, col, reverse = False):
+    sorted_list = sorted(lst, key = lambda x: x[col], reverse = reverse)
+    return (sorted_list)
+
+if __name__=="__main__":
+    path = "faxes"
+    PDFlist = listdir_with_ext(path, ".pdf")
+    # 
+    for file, date in PDFlist:
+        print (file,datetime.datetime.fromtimestamp(date))
