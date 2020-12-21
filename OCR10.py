18ecd371c65a470344309ef657f71f5af01f1db2
VinnyCarter05 <vinnycarter05@gmail.com>
2020-12-21 08:48:41 -0500

    removed print statements

--- OCR10.py
+++ OCR10.py
@@ -154,9 +154,7 @@ class MainWindow(qtw.QMainWindow, Ui_MainWindowOCR):
         self.window2.hide()
         qtw.QApplication.processEvents()
         worker = Worker(self.changePDF, self.PDF_file)
-        print(4)
         worker.start()
-        print (5)
         self.progress = 0
         high =0
         while self.progress<1.1:
@@ -165,7 +163,6 @@ class MainWindow(qtw.QMainWindow, Ui_MainWindowOCR):
                 high = self.progress
                 self.loading.progressBar.setValue(int(high * 100))
                 qtw.QApplication.processEvents()
-        print (6)
         self.label_totalPages.setText(f"of {self.numPages}")
 
         self.window2.textEdit_Preview_1.setHtml(self.curPagePreviews[self.curPage-1][0])
@@ -175,8 +172,6 @@ class MainWindow(qtw.QMainWindow, Ui_MainWindowOCR):
 
         self.setEnabled(True)
         self.window2.setEnabled(True)
-        print (7)
-        # self.changePDF(self.PDF_file)
         self.loading.close()
 
     def changePDF (self, pdf_file):
@@ -206,14 +201,11 @@ class MainWindow(qtw.QMainWindow, Ui_MainWindowOCR):
             page.save(image_name, "JPEG")
             self.progress = 0.5 + i/self.numPages/2
             i = i+1
-        print (11)
         self.setPage(1, new=True)
-        print (12)
 # self.window2.textEdit_Preview_1.setHtml(self.curPagePreviews[self.curPage-1][0])
 # self.window2.textEdit_Preview_2.setHtml(self.curPagePreviews[self.curPage-1][1])
 # self.window2.textEdit_Preview_3.setHtml(self.curPagePreviews[self.curPage-1][2])
 # self.window2.textEdit_Preview_4.setHtml(self.curPagePreviews[self.curPage-1][3])
-        print (13)
         self.progress = 2
         return
         # self.showImg (self.curImg)
